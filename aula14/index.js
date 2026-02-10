0e3e01dafbf3ae008b4e5f9ed6892c6f0a88bbe2
ere <erivaldomoreira@gmail.com>
2026-02-10 19:54:58 -0400

    aulas do dia 10/02

---
+++ aula14/index.js
@@ -0,0 +1,9 @@
+let num1 = 10;
+let num2 = 2.5;
+//console.log(num1.toString() + num2);
+//num1 = num1.toString();
+//console.log(typeof num1)
+//console.log(num1.toFixed(2))
+//console.log(Number.isInteger(num1));
+let temp = num1 * 'Olá';
+console.log(Number.isNaN(temp));
---
+++ aula15/js/index.js
@@ -0,0 +1,7 @@
+let num1 = 9.44578;
+//let num2 = Math.floor(num1);
+//let num2 = Math.ceil(num1);
+//let num2 = Math.ceil(num1);
+//let num2 = Math.round(num1);
+//console.log(Math.max(1,5,8,9,75,4));
+console.log(Math.min(1,5,8,9,75,4));
